807b31c352fd569eb14f7e2dd2fea97b12fb1127
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2019-02-25 10:59:02 +0530

    Uploading the JS File

---
+++ app.js
@@ -0,0 +1,11 @@
+/*document.getElementById('demo').innerHTML = "Hello";*/
+/*document.getElementById('demo2').style.display = "none";*/
+
+/* function to change paragraph
+function changePara(){
+  document.getElementById("demo").innerHTML = "Hey There";
+}
+*/
+
+/*document.write(5+6);*/
+
