92d8ad630bb51fc6009c53b17887b8b5382dc587
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2019-02-25 12:07:41 +0530

    Updating the JS file

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ function changePara(){
 
 /*document.write(5+6);*/
 
+/*alert("Hello");*/
+/*console.log("Hello");*/
+
+/*
+var a = 10;
+var b = 4;
+var c = a + b;
+
+document.getElementById("demo").innerHTML = c;
+*/
+
+/*
+function myFunction(){
+document.getElementById("para1").innerHTML = "Hey There"
+;
+document.getElementById("para2").innerHTML = "Hello World"
+;
+}
+*/
